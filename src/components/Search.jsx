a743d2d67a1c72278d7d05b2ab72d0ec7d67881c
Ria0905 <115111805+Ria0905@users.noreply.github.com>
2025-11-07 02:12:37 +0100

    Initial commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-md-6'>
+                <img src='images/office-style.png' alt='' className='img-fluid'/>Dresses</div>
+            <div className='col-md-6'>
+                <img src='images/office-style.png' alt='' className='img-fluid'/>Pants</div>
+            <div className='col-md-12'>
+                <img src='images/accs.jpg' alt='' className='img-fluid'/>Accessories</div>
+            <div className='col-md-6'>
+                <img src="images/shoes.jpg" alt="" className='img-fluid'/> Shoes</div>
+            <div className='col-md-6'>
+                <img src="" alt="" /> T-Shirts</div>
+            
+        </div>
+        <img src="images/dresses.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Search
